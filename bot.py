5c74497f3d2dbcfa9bdd4ba7dab83e056cde5f1d
TrishaantSarin <sarin3@illinois.edu>
2021-06-07 22:59:47 +0530

    Implemented MVP + Sprinkles=

--- bot.py
+++ bot.py
@@ -1,9 +1,13 @@
 from discord.ext import commands
+from discord.utils import get
+import discord
 import random
 
 bot = commands.Bot(command_prefix='!')
 
-@bot.command(name="idea")
+curses = ['yeo']
+
+@bot.command(name="idea", help="Get an idea for a Side Project!")
 async def idea(ctx):
     await ctx.send("Ideas are hard")
     await ctx.send("Worry not, I think you should...")
@@ -15,7 +19,7 @@ async def idea(ctx):
     idea = f'Create a new {random.choice(topics)} that helps with {random.choice(areas)} in {random.choice(lang)}! :slight_smile:'
     await ctx.send(idea)
 
-@bot.command(name="calc")
+@bot.command(name="calc", help="Perform a calculation where fn is either +,-,*,**, or /")
 async def calc(ctx, x, fn, y):
     x = float(x)
     y = float(y)
@@ -32,8 +36,109 @@ async def calc(ctx, x, fn, y):
     else:
         await ctx.send("We only support 5 function operations")
 
-@bot.command(name="idea", help="Get a side project idea")
-@bot.command(name="calc", help="Perform a calculation where fn is either +,-,*,**, or /")
+@bot.command(name="dog", help="Return information about certain breeds of dogs")
+async def dog(ctx, fn = None):
+    if fn == None:
+        await ctx.send("No input. Try again with an input")
+
+    breeds = {
+        "kohaku": "The Creator's dog; the best doggo in existence. 9999/10",
+        "labrador": "Medium doggo, very fun and cute. 9/10",
+        "daschund": "Small, long doggo, sausage doggo is good doggo. 8/10",
+        "chihuahua": "Tiny animal, loud and annoying. 0/10",
+    }
+
+    fnS = fn.lower()
+
+    if breeds.get(fnS) == None:
+        await ctx.send("This doggo does not exist or is not yet supported. Recheck spelling or try other doggo")
+    else:
+        await ctx.send(breeds.get(fnS))
+
+@bot.command(name="curse")
+async def curse(ctx, com = None, cword = None):
+    if com.lower() == "add":
+        curses.append(cword.lower())
+        await ctx.send(cword + " added to curse list")
+    elif com.lower == "del" or com.lower() == "delete":
+        curses.remove(cword)
+    elif com.lower == "dis" or com.lower() == "display":
+        # await ctx.send("There are " + curses.count() + "curses")
+        for word in curses:
+            await ctx.send(word)
+    elif com == None and cword == None:
+        await ctx.send("Invalid curse command")
+
+@bot.command(pass_context=True)
+@commands.has_role("Admin") 
+async def giverole(ctx, user: discord.Member, role: discord.Role):
+    if not get(ctx.guild.roles, role):
+        await ctx.send(f"hey @{ctx.author.name}, this role does not exist. Try !addrole to create a new role!")
+    await user.add_roles(role)
+    await ctx.send(f"hey {ctx.author.name}, {user.name} has been giving a role called: {role.name}")
+
+@bot.command(pass_context=True)
+@commands.has_role("Admin") 
+async def addrole(ctx, role: str):
+    guild = ctx.guild
+    await guild.create_role(name=role)
+    await ctx.send(f"hey @{ctx.author.name}, {role} has been created")
+
+@bot.command()
+@commands.has_permissions(ban_members = True)
+@commands.has_role("Admin")
+async def ban(ctx, member :  discord.Member, *,reason=None):
+    if member == None or member == ctx.message.author:
+        await ctx.channel.send("You cannot ban yourself")
+        return
+    if reason == None:
+        reason = "For being a jerk! (Reason not provided)"
+    message = f"You have been banned from {ctx.guild.name} for {reason}"
+    await member.send(message)
+    await member.ban(reason=reason)
+    await ctx.send(f"{member} is banned!")
+
+@bot.command()
+@commands.has_permissions(ban_members = True)
+@commands.has_role("Admin")
+async def kick(ctx, member :  discord.Member, *,reason=None):
+    if member == None or member == ctx.message.author:
+        await ctx.channel.send("You cannot ban yourself")
+        return
+    if reason == None:
+        reason = "For being a jerk! (Reason not provided)"
+    message = f"You have been kicked from {ctx.guild.name} for {reason}"
+    await member.send(message)
+    await member.kick(reason=reason)
+    await ctx.send(f"{member} is kicked!")
+
+@bot.command()
+@commands.has_permissions(ban_members = True)
+@commands.has_role("Admin")
+async def warn(ctx, member :  discord.Member, *,reason=None):
+    if member == None or member == ctx.message.author:
+        await ctx.channel.send("You cannot ban yourself")
+        return
+    if reason == None:
+        reason = "For being a jerk! (Reason not provided)"
+    message = f"You have been warned for {reason} for {ctx.guild.name}"
+    await member.send(message)
+    await ctx.send(f"{member} has been warned")
+
+
+@bot.listen()
+async def on_message(message):
+    if message.author.id == bot.user.id:
+        return
+    if message.content.startswith("!"):
+        return
+    msg_content = message.content.lower()
+
+    curseWord = curses
+    
+    # delete curse word if match with the list
+    if any(word in msg_content for word in curseWord):
+        await message.delete()
 
 # make sure to create a token file (in real life use env variables)
 with open("BOT_TOKEN.txt", "r") as token_file:
